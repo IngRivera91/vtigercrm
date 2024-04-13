46a316d36d2f702363d39e7f7cf492cfc4a7205e
Ricardo Rivera <ricardo@ingrivera.com>
2024-04-12 22:37:42 -0600

    new param int the config vtapi

--- example.config.vtapi.php
+++ example.config.vtapi.php
@@ -1,6 +1,7 @@
 <?php
-class ConfigVtapi
+class ConfigurationVtapi
 {
     public static $username = "admin";
     public static $accessKey = "";
+    public static $url = "http://localhost/vtigercrm/webservice.php";
 }
