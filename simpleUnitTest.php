54838482f7aaa34d622adbb3fae717036ba11b15
Ricardo Rivera <ricardo@ingrivera.com>
2024-04-13 12:17:28 -0600

    add new file to test classes

---
+++ simpleUnitTest.php
@@ -0,0 +1,67 @@
+<?php
+
+require_once 'vendor/autoload.php';
+require_once 'config.apiconekta.php';
+require_once 'vtapi/VtapiConnection.php';
+require_once 'vtapi/VtapiContacts.php';
+require_once 'conekta/ApiConektaCustomers.php';
+
+function DPrint($variable)
+{
+    echo '<pre>';
+    print_r($variable);
+    echo '</pre>';
+}
+
+
+$connection = new VtapiConnection();
+$sessionName = $connection->getSessionName();
+$userId = $connection->getUserId();
+
+if ($userId !== "") {
+//    DPrint($userId);
+    DPrint('success');
+}else {
+    DPrint('failure');
+}
+
+if ($sessionName !== "") {
+//    DPrint($sessionName);
+    DPrint('success');
+}else {
+    DPrint('failure');
+}
+
+$username = ConfigurationVtapi::$username;
+try {
+    ConfigurationVtapi::$username = "bad_username";
+    $connection = new VtapiConnection();
+    DPrint('failure');
+} catch (Exception $e) {
+    ConfigurationVtapi::$username = $username;
+//    DPrint($e->getMessage());
+    if ($e->getMessage() === "Error trying to generate the Token") {
+        DPrint('success');
+    }else{
+        DPrint('failure');
+    }
+}
+
+$accessKey = ConfigurationVtapi::$accessKey;
+try {
+    ConfigurationVtapi::$accessKey = "bad_access_key";
+    $connection = new VtapiConnection();
+    DPrint('failure');
+} catch (Exception $e) {
+    ConfigurationVtapi::$accessKey = $accessKey;
+//    DPrint($e->getMessage());
+    if ($e->getMessage() === "Error trying to generate the SessionName and UserId") {
+        DPrint('success');
+    }else{
+        DPrint('failure');
+    }
+}
+
+
+
+
