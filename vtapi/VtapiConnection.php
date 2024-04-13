cb672ad548fc07e30f9d3a7bbdfc3c2b74965c5a
Ricardo Rivera <ricardo@ingrivera.com>
2024-04-12 23:11:19 -0600

    class VtapiConnection done

--- vtapi/VtapiConnection.php
+++ vtapi/VtapiConnection.php
@@ -4,25 +4,41 @@ use GuzzleHttp\Exception\GuzzleException;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 require dirname(__DIR__).'/config.vtapi.php';
+
 class VtapiConnection
 {
+    private $client;
     private $token;
     private $sessionName;
     private $username;
     private $accessKey;
     private $url;
 
+    /**
+     * @throws Exception
+     */
     public function __construct()
     {
         $this->username = ConfigurationVtapi::$username;
         $this->accessKey = ConfigurationVtapi::$accessKey;
         $this->url = ConfigurationVtapi::$url;
-        $this->getToken();
 
-        print_r($this->token);
+        if (!$this->generateToken()) {
+            throw new Exception('Error trying to generate the Token');
+        }
+
+        if (!$this->generateSessionName()) {
+            throw new Exception('Error trying to generate the SessionName');
+        }
+
     }
 
-    private function getToken(): bool
+    public function getSessionName()
+    {
+        return $this->sessionName;
+    }
+
+    private function generateToken(): bool
     {
         $client = new GuzzleHttp\Client();
 
@@ -38,5 +54,34 @@ class VtapiConnection
 
         return true;
 
+    }
+
+    private function generateSessionName(): string
+    {
+        $client = new GuzzleHttp\Client();
+
+        $headers = [
+            'Content-Type' => 'application/x-www-form-urlencoded'
+        ];
+
+        $options = [
+            'form_params' => [
+                'operation' => 'login',
+                'username' => $this->username,
+                'accessKey' => md5($this->token.$this->accessKey),
+            ]
+        ];
+
+        $request = new GuzzleHttp\Psr7\Request('POST', "{$this->url}", $headers);
+
+        try {
+            $response = $client->send($request, $options);
+        } catch (GuzzleException $e) {
+            return false;
+        }
+
+        $this->sessionName = json_decode($response->getBody())->result->sessionName;
+
+        return true;
     }
 }
