6582f5546ff4689607c857c898fd5ba254bcc771
Ricardo Rivera <ricardo@ingrivera.com>
2024-04-13 13:21:07 -0600

    relsolve issues in handler CrearCustomerConektaHandler

--- modules/Contacts/CrearCustomerConektaHandler.php
+++ modules/Contacts/CrearCustomerConektaHandler.php
@@ -15,31 +15,37 @@ class CrearCustomerConektaHandler extends VTEventHandler
         $moduleName = $entity->getModuleName();
         if ($moduleName == 'Contacts' && $eventType == 'vtiger.entity.beforesave') {
 
-            $ConektaCustomer = new ApiConektaCustomers();
+            $createdTime = $entity->get('createdtime');
+            $source = $entity->get('source');
 
-            $otherPhone = $entity->get('otherphone');
-            $firstName = $entity->get('firstname');
-            $email = $entity->get('email');
-            $phone = $entity->get('phone');
+            if ($createdTime !== "" || $source !== "WEBSERVICE") {
 
-            if ($otherPhone === "") {
-                try {
-                    $otherPhone = $ConektaCustomer->createCustomer($firstName, $email, $phone);
-                    $entity->set('otherphone', $otherPhone);
-                } catch (Exception $e) {
-                    throw new Exception($e->getMessage());
-                }
-            }
+                $ConektaCustomer = new ApiConektaCustomers();
 
-            if ($otherPhone !== "") {
-                if ($ConektaCustomer->customerExist($otherPhone)) {
+                $otherPhone = $entity->get('otherphone');
+                $firstName = $entity->get('firstname');
+                $email = $entity->get('email');
+                $phone = $entity->get('phone');
+
+                if ($otherPhone === "") {
                     try {
-                        $ConektaCustomer->updateCustomer($otherPhone, $firstName, $email, $phone);
+                        $otherPhone = $ConektaCustomer->createCustomer($firstName, $email, $phone);
+                        $entity->set('otherphone', $otherPhone);
                     } catch (Exception $e) {
                         throw new Exception($e->getMessage());
                     }
-                }else{
-                    throw new Exception('No existe un cliente con el ID ' . $otherPhone);
+                }
+
+                if ($otherPhone !== "") {
+                    if ($ConektaCustomer->customerExist($otherPhone)) {
+                        try {
+                            $ConektaCustomer->updateCustomer($otherPhone, $firstName, $email, $phone);
+                        } catch (Exception $e) {
+                            throw new Exception($e->getMessage());
+                        }
+                    }else{
+                        throw new Exception('No existe un cliente con el ID ' . $otherPhone);
+                    }
                 }
             }
 
