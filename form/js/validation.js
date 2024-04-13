8b69ffadf55f442eb779a2161629da9635e6b314
Ricardo Rivera <ricardo@ingrivera.com>
2024-04-12 18:37:16 -0600

    add new rules for valid email and phonenumber

--- form/js/validation.js
+++ form/js/validation.js
@@ -3,6 +3,8 @@ function validaFormulario() {
 
     let expRegNombre= /^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
     let expRegApellidos= /^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+    var expRegCorreo=/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    var expRegTelefono= /^[\(]?[\+]?(\d{2}|\d{3})[\)]?[\s]?((\d{6}|\d{8})|(\d{3}[\*\.\-\s]){3}|(\d{2}[\*\.\-\s]){4}|(\d{4}[\*\.\-\s]){2})|\d{8}|\d{10}|\d{12}$/;
 
     let formulario = document.getElementById("form");
     let nombre = document.getElementById("first_name");
@@ -40,8 +42,20 @@ function validaFormulario() {
         validar = false;
     }
 
+    else if (!expRegCorreo.test(correo.value) && validar) {
+        alert("Ingrese un correo valido");
+        correo.focus();
+        validar = false;
+    }
+
     else if (!telefono.value && validar) {
         alert("El campo telefono es requerido");
+        telefono.focus();
+        validar = false;
+    }
+
+    else if (!expRegTelefono.test(telefono.value) && validar) {
+        alert("Ingrese un telefono valido");
         telefono.focus();
         validar = false;
     }
